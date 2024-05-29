84e6a26540278ccaf3d43112b3823adc23cd53db
Rrdsrd <rishav.dash2012@gmail.com>
2024-05-29 19:42:54 +0530

    Project Structure

---
+++ template.py
@@ -0,0 +1,42 @@
+import os
+from pathlib import Path
+import logging
+
+logging.basicConfig(level=logging.INFO)
+project_name="mlproject"
+list_of_files=[
+    ".github/workflows/.gitkeep",
+    f"src/{project_name}/__init__.py",
+    f"src/{project_name}/components/__init__.py",
+    f"src/{project_name}/components/data_injection.py",
+    f"src/{project_name}/components/data_transformation.py",
+    f"src/{project_name}/components/model_trainer.py",
+    f"src/{project_name}/pipeleines/__init__.py",
+    f"src/{project_name}/pipeleines/training_pipeline.py",
+    f"src/{project_name}/pipeleines/prediction_pipeline.py",
+    f"src/{project_name}/exception.py",
+    f"src/{project_name}/logger.py",
+    f"src/{project_name}/utils.py",
+    "main.py",
+    "app.py",
+    "Dockerfile",
+    "requirements.txt",
+    "setup.py",
+    
+]
+
+for i in list_of_files:
+    file_path=Path(i)
+    file_dir,file_name=os.path.split(file_path)
+    
+    if  file_dir!="":
+        os.makedirs(file_dir,exist_ok=True)
+        logging.info(f"creating directory:{file_dir} for the file {file_name}")
+        
+    if (not os.path.exists(file_path)) or (os.path.getsize(file_path)==0):
+        with open(file_path,'w') as f:
+            pass
+            logging.info(f"creating empty file:{file_path}")
+            
+    else:
+        logging.info(f"{file_name} already exists")
